14a89fe21e4e9aae6f6ed2b8e50627ad6b76557b
bishalpokharel325 <bishalpokharel325@gmail.com>
2020-08-26 22:30:50 +0545

    added comments

--- server.py
+++ server.py
@@ -1,9 +1,14 @@
+# importing necessary modules
 from flask import Flask
+# creating a python flask application
 app=Flask(__name__)
+
 @app.route("/")
 def index():
     return "<h1>hello world</h1>"
+# initialize server
 if __name__ == '__main__':
     app.run(debug=True)
+
 
 
